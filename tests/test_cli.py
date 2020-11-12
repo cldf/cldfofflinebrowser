aecd0c790ccc523af7458204924d4bc9815f1ce9
xrotwang <xrotwang@googlemail.com>
2020-11-12 16:20:35 +0100

    more tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,4 +4,9 @@ from cldfbench.__main__ import main
 
 
 def test_create(tmpdir):
-    main(['offline.create', str(pathlib.Path(__file__).parent / 'dataset' / 'dataset.py'), '--outdir', str(tmpdir)])
+    out = pathlib.Path(str(tmpdir))
+    ds = pathlib.Path(__file__).parent / 'dataset' / 'dataset.py'
+    main(['offline.create', str(ds), '--outdir', str(out)])
+    assert not out.joinpath('parameter-1', 'ask.wav').exists()
+    main(['offline.create', str(ds), '--outdir', str(out), '--with-audio'])
+    assert out.joinpath('parameter-1', 'ask.wav').exists()
